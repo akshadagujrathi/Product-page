783c9ea50cfb1409f21f3008d2d29f2cf1d1659f
Akshada Gujarathi <120277114+akshadagujrathi@users.noreply.github.com>
2024-01-17 13:53:21 +0530

    Add files via upload

---
+++ Main-product/index.js
@@ -0,0 +1,319 @@
+let product = [
+    {
+        id: 1,
+        product_img: "./images/MissClair_Illuminator.jpg",
+        alternate_img: [
+            {
+                image:"./images/Revlon Makeup.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Miss Claire Born To Glow Illuminator Make-up Base (05 Shiny)",
+        price: "240",
+        category:"Beauty",
+        seller: "Miss Clair",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 2,
+        product_img: "./images/Revlon Makeup.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Revlon Touch and Glow Liquid Make Up, Sand Mist/20ml",
+        price: "296",
+        category:"Beauty",
+        seller: "Revlon",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 3,
+        product_img: "./images/bollywood_flutter.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "RENEE Bollywood Filter Face Primer 15gm",
+        price: "376",
+        category:"Beauty",
+        seller: "RENEE",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 4,
+        product_img: "./images/facecan_powder.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "FACES CANADA Weightless Stay Matte Finish Compact Powder",
+        price: "112",
+        category:"Beauty",
+        seller: "FACES CANADA",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 5,
+        product_img: "./images/Lak_rose_powder.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Maybelline New York Fit Me Matte + Poreless Compact Powder",
+        price: "170",
+        category:"Beauty",
+        seller: "Maybelline",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 6,
+        product_img: "./images/noise_pulse.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Noise Pulse 2 Max 1.85 Display, Bluetooth Calling Smart Watch, Smartwatch for Men and Women (Jet Black)",
+        price: "1299",
+        category:"Electronics",
+        seller: "Noise",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 7,
+        product_img: "./images/sport_watch.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Shocknshop Digital Sports Stylish Multifunctional Electronic LED Black Dial Wrist Watch for Men Boys -WCH78",
+        price: "499",
+        category:"Electronics",
+        seller: "Shocknshop",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 8,
+        product_img: "./images/hp_lapi.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "HP Laptop 15s, Intel Celeron, 15.6-inch (39.6 cm), HD, 8GB DDR4, 512GB SSD, Intel UHD Graphics, Thin & Light",
+        price: "29000",
+        category:"Electronics",
+        seller: "HP",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 9,
+        product_img: "./images/ponds_bb.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "POND'S Bb+ Cream, Instant Spot Coverage + Light Make-Up Glow, Ivory 30G, Natural",
+        price: "220",
+        category:"Electronics",
+        seller: "POND'S",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 10,
+        product_img: "./images/Apple_mac.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Apple 2023 MacBook Air Laptop with M2 chip: 38.91 cm (15.3-inch) Liquid Retina Display, 8GB RAM, 256GB SSD Storage",
+        price: "1,19000",
+        category:"Electronics",
+        seller: "POND'S",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 11,
+        product_img: "./images/tehno_mac.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "TECNO MEGABOOK T1,Intel Core 11th Gen i7 Processor (16GB RAM/1TB SSD Storage), 15.6-Inch (39.62 CM) Eye Comfort disply",
+        price: "35000",
+        category:"Electronics",
+        seller: "TECNO",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 12,
+        product_img: "./images/jerkin.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Women's Cotton Hooded Neck Sweatshirt",
+        price: "619",
+        category:"Clothing",
+        seller: "Dhruvi",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 13,
+        product_img: "./images/clothing2.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "Lifestyle Cotton Track Suit Men's Co-ord Sets",
+        price: "499",
+        category:"Clothing",
+        seller: "Raymond",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    },
+    {
+        id: 14,
+        product_img: "./images/baby-cloths.jpg",
+        alternate_img: [
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            },
+            {
+                image:"./images/MissClair_Illuminator.jpg"
+            }
+                    ],
+        product_name: "unisex child fleece Track Suit",
+        price: "375",
+        category:"Clothing",
+        seller: "Roaster",
+        Description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate numquam repudiandae sapiente error possimus iste ad repellat id quia minima?"
+    }
+];
+
+ 
+function productList(){
+    const product_item_container = document.getElementById('product-item-container');
+    product.forEach(myfunc);
+    function myfunc(value){
+       const product_item = document.createElement('div'); 
+       product_item.classList.add("product-item");
+       const main_product = document.createElement('div');
+       main_product.classList.add("main-product")
+     
+       main_product.innerHTML = ` <img class="productImage" src="${value.product_img}" alt="${product.name}">
+       <h3 class="productname">${value.product_name}</h3>
+       <h6><span>₹</span> ${value.price}</h6>
+       `
+       
+       product_item.addEventListener('click', () => single_product(value));
+    //    document.getElementById('product-item-container').appendChild(product_item);
+   
+    product_item_container.appendChild(product_item);
+    product_item.appendChild(main_product);
+    
+   
+   };
+    }
+
+function single_product(product){
+   
+    const single_product = document.getElementById('single-product');
+    const product_item_container = document.getElementById('product-item-container');
+    const product_content = document.getElementById('product-content');
+    const singleProductDetails = document.getElementById('singleProductDetails');
+    const product_images = document.getElementById('product-images');
+    const variantGalleryDiv = document.getElementById('variantGallery');
+    
+    const mainProductImage = document.getElementById('mainProductImage');
+    const thumbImage = document.getElementById('thumb-img');
+    
+    singleProductDetails.innerHTML = `
+    <h3 class="productname">${product.product_name}</h3>
+            <p>${product.Description}</p>
+            <p><b>Category:</b> ${product.category}</p>
+            <p><b>Seller:</b> ${product.seller}</p>
+            <h6> <span>₹</span> ${product.price}</h6>
+`
+
+mainProductImage.src = product.product_img;
+
+            variantGalleryDiv.innerHTML = '';
+      product.alternate_img.forEach(variant => {
+        const variantImage = document.createElement('img');
+        variantImage.src = variant.image;
+        variantImage.classList.add('variantImage');
+        variantImage.addEventListener('click', () => changeProductImage(variant.image));
+        variantGalleryDiv.appendChild(variantImage);
+      });
+     
+    thumbImage.src = product.alternate_img[0].image;
+
+single_product.appendChild(product_content);
+single_product.appendChild(product_images);
+product_content.appendChild(singleProductDetails);
+product_images.appendChild(mainProductImage);
+product_images.appendChild(variantGalleryDiv);
+
+    product_item_container.style.display = 'none';
+    single_product.style.display = 'block';
+
+
+}
+
+function changeProductImage(imageUrl) {
+    const mainProductImage = document.getElementById('mainProductImage');
+    mainProductImage.src = imageUrl;
+  }
+
+    
+
+        productList();
